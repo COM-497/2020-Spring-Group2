56febe226bf5b33edae5fb53382974a305126be7
patberry99 <55572395+patberry99@users.noreply.github.com>
2020-03-24 14:38:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-var path = __dirname + '/';
+var path = __dirname + '/views/';
 
 app.use('/',router);
-
 app.use(express.static('views'));
+
 router.get('/',function(req,res){
     res.sendFile(path+'index.html');
 });
@@ -21,6 +21,9 @@ router.get('/team',function(req,res){
 
 router.get('/product',function(req,res){
     res.sendFile(path+'product.html');
+});
+router.get('/login',function(req,res){
+    res.sendFile(path+'login.html');
 });
 
 app.listen(3000,function(){

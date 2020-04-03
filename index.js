297ca878eff2249a418d15e336bb89ba31910dce
patberry99 <55572395+patberry99@users.noreply.github.com>
2020-04-03 11:25:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const sendMail = require('./mail');
 const app = express();
 var router = express.Router();
 const path = require('path')
-const nodemailer = require('nodemailer')
+
+
 
 //Data parsing: when html gets data, able to process 
 app.use(express.urlencoded({extended: false}));
@@ -10,16 +12,27 @@ app.use(express.json());
 
 //recieving data from client using /email route
 app.post('/email',(req,res) => {
-    //TODO:
-    //send email here 
-    console.log('Data:' ,req.body);
+    const {subject, email, text} = req.body;
+    sendMail(email, subject, text,function(err,data){
+        if(err){
+            res.status(500).json({message: "Internal Error: Please Contact via Email"});
+        }
+        else{
+            res.json({message:"Message has been recieved."});
+        }
+});
     res.json({message: "Message received!"})
+  // res.redirect('/contact-success');
 });
 
 var path1 = __dirname + '/views/';
 
 app.use('/',router);
 app.use(express.static('views'));
+
+//app.get('/contact-success',function(req,res){
+  //  res.sendFile(path.join(__dirname,'views','contact-success.html'));
+//});
 
 router.get('/',function(req,res){
     res.sendFile(path1+'index.html');

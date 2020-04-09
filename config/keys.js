6d16761ad72f822d49ed8806e24893bd93e78a47
patberry99 <55572395+patberry99@users.noreply.github.com>
2020-04-09 15:46:31 -0400

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//module.exports = {
+  //  MongoURI: 'mongodb+srv://PatrickBerry:BerryPatrick01@login-lctdw.mongodb.net/test?retryWrites=true&w=majority'
+//}
+
+//connecting to the MongoDB 
+
+module.exports = {
+   MongoURI: 'mongodb+srv://patrick:patrick12345@cluster1-fcux6.mongodb.net/test?retryWrites=true&w=majority'
+}
